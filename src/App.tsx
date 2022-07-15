cff7b43db48c8d9956587540fd8417edaa135f0a
BloodCryHiro <jo090989coding@gmail.com>
2022-07-16 03:16:38 +0800

    feat: create header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+// Custom components
+import Header from "./components/Header";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div className="bg-slate-600 h-screen">
+      <Header />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <h1 className="text-slate-300 text-3xl font-bold p-5">
+      MY SUPER COUNTERS!!!
+    </h1>
+  );
+};
+
+export default Header;
